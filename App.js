ef86e1d4a7a0ebccc755e9d559a0903995f4c546
sekhar gunda <sekhar.g0505@gmail.com>
2026-02-12 12:39:48 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div", { id :"parent" },
+    React.createElement("div", { id : "child" },
+        React.createElement("h1",{},"I'm heading tag")
+    )
+ );
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
