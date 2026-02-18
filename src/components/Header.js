44ab67d16429de2bc400e631d5d1cf3ad52baada
sekhar gunda <sekhar.g0505@gmail.com>
2026-02-18 06:41:30 +0530

    creation of source file

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="Logo-container">
+                <img className="logo"
+                src="./utils/logo.png"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
